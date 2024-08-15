161cceba6f8c247b77eeade2a5c2abbb5afc3442
Somaiya Shahin <somaiyashahin484@gmail.com>
2024-08-15 23:28:28 +0600

    top nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/topweb.png" type="image/x-icon" className="w-full" />
+        {/* <link rel="icon" href="/" type="image/x-icon" className="w-full" /> */}
+        <link rel="topIcon" href="/topweb.png" type="image/x-icon" className="w-full" />
       </head>
 
       <body className={inter.className}>
