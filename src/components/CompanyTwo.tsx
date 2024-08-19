1df938a1cf695a2187f53a4c06d9caceb612ef1e
Somaiya Shahin <somaiyashahin484@gmail.com>
2024-08-19 14:51:27 +0600

    cross

--- src/components/CompanyTwo.tsx
+++ src/components/CompanyTwo.tsx
@@ -1,16 +1,16 @@
 import Marquee from "react-fast-marquee";
 import Image from 'next/image';
-import com1 from '../../public/com1.png'
-import com2 from '../../public/com2.png'
-import com3 from '../../public/com3.png'
-import com4 from '../../public/com4.png'
-import com5 from '../../public/com5.png'
-import com6 from '../../public/com6.png'
-import com7 from '../../public/com7.png'
-import com8 from '../../public/com8.jpg'
-import com9 from '../../public/com9.png'
-import com10 from '../../public/com10.jpg'
-import com11 from '../../public/com11.png'
+// import com1 from '../../public/com1.png'
+// import com2 from '../../public/com2.png'
+// import com3 from '../../public/com3.png'
+// import com4 from '../../public/com4.png'
+// import com5 from '../../public/com5.png'
+// import com6 from '../../public/com6.png'
+// import com7 from '../../public/com7.png'
+// import com8 from '../../public/com8.jpg'
+// import com9 from '../../public/com9.png'
+// import com10 from '../../public/com10.jpg'
+// import com11 from '../../public/com11.png'
 
 const CompanyTwo: React.FC = () => {
     return (
@@ -25,24 +25,24 @@ const CompanyTwo: React.FC = () => {
             </div>
 
             <Marquee pauseOnHover speed={100} direction="left" className='order g-4 hover:cursor-pointer'>
-                <div className="flex items-center md:gap-36 ">
+                {/* <div className="flex items-center md:gap-36 ">
                     <Image src={com1} width={250} height={250} alt="Company Logo" className="w-52 md:ml-24" />
                     <Image src={com2} width={250} height={250} alt="Company Logo" className="w-40" />
                     <Image src={com3} width={250} height={250} alt="Company Logo" className="w-52" />
                     <Image src={com4} width={250} height={250} alt="Company Logo" className="w-52" />
                     <Image src={com5} width={250} height={250} alt="Company Logo" className="w-44" />
                     <Image src={com6} width={250} height={250} alt="Company Logo" className="w-52" />
-                </div>
+                </div> */}
             </Marquee>
 
             <Marquee pauseOnHover speed={100} direction="right" className='order g-4 mt-14 hover:cursor-pointer'>
-                <div className="flex items-center md:gap-36">
+                {/* <div className="flex items-center md:gap-36">
                     <Image src={com7} width={250} height={250} alt="Company Logo" className="w-52 md:ml-24" />
                     <Image src={com8} width={250} height={250} alt="Company Logo" className="w-40" />
                     <Image src={com9} width={250} height={250} alt="Company Logo" className="w-52" />
                     <Image src={com10} width={250} height={250} alt="Company Logo" className="w-52" />
-                    <Image src={com11} width={250} height={250} alt="Company Logo" className="w-52" />
-                </div>
+                    <Image src={com11} width={250} height={250} alt="Company Logo" className="w-52" /> */}
+                {/* </div> */}
             </Marquee>
 
         </div>
